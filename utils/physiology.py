87675a16f921cc813737a864b1e053595ac2b592
Julia Wang <jwang22@stanford.edu>
2018-12-12 23:10:47 -0800

    physiology code, not tested

--- utils/physiology.py
+++ utils/physiology.py
@@ -1,7 +1,27 @@
 import torch
 import torch.nn as nn
+#Use: phys = Physio(myNet)
+#	  activity_dict = phys.inspect(stim)
+#     activity_dict.['conv2d_1'] <--- gets the conv2d_1 layer activity
 
-def hook(module, inp, out):
-    return
+class Physio:
+	def __init__(self, net):
+		self.net = net
+		self.dict = {};
+
+	def layer_activity(self, module, inp, out):
+		self.dict[module.__class__.__name__] = out
+
+	def inspect(self, stim):
+		for module in self.net.modules():
+			module.register_forward_hook(layer_activity)
+		self.dict['output'] = self.net(stim)
+		return self.dict
+
+
+
+
+
+
 
 
