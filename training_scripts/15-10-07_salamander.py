38e9c4835205284e1cf510e64747644f3318081f
Julia Wang <jwang22@stanford.edu>
2019-02-05 17:22:57 -0800

    added trained models

--- training_scripts/15-10-07_salamander.py
+++ training_scripts/15-10-07_salamander.py
@@ -14,6 +14,7 @@ sys.path.append('../models/')
 sys.path.append('../utils/')
 from BN_CNN import BNCNN
 import retio as io
+import argparse
 
 from deepretina.experiments import loadexpt
 
@@ -24,9 +25,9 @@ DEVICE = torch.device("cuda:0")
 # Load data using Lane and Nirui's dataloader
 train_data = loadexpt('15-10-07',[0,1,2,3,4],'naturalscene','train',40,0)
 test_data = loadexpt('15-10-07',[0,1,2,3,4],'naturalscene','test',40,0)
-val_split = 500
+val_split = 0.005
 
-def main(model_class,epochs=200,batch_size=1000,LR=1e-4,l2_scale=0.01,l1_scale=0.01,shuffle=False):
+def train(model_class,epochs=200,batch_size=1000,LR=1e-3,l2_scale=0.01,l1_scale=0.01,shuffle=False, save='~/'):
     LAMBDA1 = l1_scale
     LAMBDA2 = l2_scale
     EPOCHS = epochs
@@ -48,15 +49,15 @@ def main(model_class,epochs=200,batch_size=1000,LR=1e-4,l2_scale=0.01,l1_scale=0
         if shuffle:
             print('shuffling data...')
             np.random.shuffle(epoch_tv_x)
-            np.random.shuffle(epoch_train_y)
+            np.random.shuffle(epoch_tv_y)
             print('data shuffled!')
 
         # train/val split
-        val_ind = 500
-        epoch_train_x = epoch_tv_x[val_ind:]
-        epoch_val_x = epoch_tv_x[:val_ind]
-        epoch_train_y = epoch_tv_y[val_ind:]
-        epoch_val_y = epoch_tv_y[:val_ind]
+        num_val = 2000
+        epoch_train_x = epoch_tv_x[num_val:]
+        epoch_val_x = epoch_tv_x[:num_val]
+        epoch_train_y = epoch_tv_y[num_val:]
+        epoch_val_y = epoch_tv_y[:num_val]
         epoch_length = epoch_train_x.shape[0]
         num_batches,leftover = divmod(epoch_length, BATCH_SIZE)
         batch_size = BATCH_SIZE
@@ -97,9 +98,42 @@ def main(model_class,epochs=200,batch_size=1000,LR=1e-4,l2_scale=0.01,l1_scale=0
 
         #validate model
         val_obs = model(epoch_val_x.to(DEVICE)).cpu().detach().numpy()
-        val_loss = np.sum([pearsonr(val_obs[:, i], epoch_val_y[:, i]) for i in range(epoch_val_y.shape[-1])])
+        val_acc = np.sum([pearsonr(val_obs[:, i], epoch_val_y[:, i]) for i in range(epoch_val_y.shape[-1])])
         scheduler.step(val_loss)
-        io.save_checkpoint(model,epoch,epoch_loss,optimizer,'~/','test')
+        io.save_checkpoint(model,epoch,epoch_loss/num_batches,optimizer,save,'test')
+    return val_acc
+
+def hyperparameter_search(param, values):
+    best_val_acc = 0
+    best_val = None
+    for val in values:
+        save = '~/julia/torch-deepretina/Trained_1/29/18_{0}_{1}'.format(param, val)
+        if param == 'batch_size':
+            val_acc = train(BNCNN, batch_size=val, save=save)
+        elif param == 'lr':
+            val_acc = train(BNCNN, LR=val, save=save)
+        elif param == 'l2':
+            val_acc = train(BNCNN, l2_scale=val, save=save)
+        elif param == 'l2':
+            val_acc = train(BNCNN, l1_scale=val, save=save)
+        if val_loss > best_val_loss:
+            best_val_acc = val_acc
+            best_val = val
+    print("The best valuation loss achieved was {0} with a {1} value of {2}".format(best_val_loss, param, best_val))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', default = 200)
+    parser.add_argument('--batch', default = 1000)
+    parser.add_argument('--lr', default = 1e-3)
+    parser.add_argument('--l2', default = 0.01)
+    parser.add_argument('--l1', default = 0.01)
+    parser.add_argument('--shuffle', action='store_true')
+    parser.add_argument('--save', default='~/')
+    args = parser.parse_args(sys.argv[1:])
+    train(BNCNN, args.epochs, args.batch, args.lr, args.l2, args.l1, args.shuffle, args.save)
+
 
 if __name__ == "__main__":
-    main(BNCNN)
+    main()
