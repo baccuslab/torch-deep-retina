248973d469eb325c84c3ba9b3a485ab7ee41400f
Satchel Grant <grantsrb@gmail.com>
2019-03-20 14:31:59 -0700

    remove unnecessary comments

--- utils/miscellaneous.py
+++ utils/miscellaneous.py
@@ -1,18 +1,5 @@
 import numpy as np
 
-#def parallel_shuffle(*args, perm=None, ret_perm=False):
-#    shuffles = [np.empty(args[0].shape, dtype=args[0].dtype)]
-#    for i in range(len(args[1:])):
-#        assert len(args[i-1]) == len(args[i])
-#        shuffles.append(np.empty(args[i].shape, dtype=args[i].dtype))
-#    if perm is None:
-#        perm = np.random.permutation(len(args[0]))
-#    for old_idx, new_idx in enumerate(perm):
-#        for arr_idx in range(len(shuffles)):
-#            shuffles[arr_idx][new_idx] = args[arr_idx][old_idx]
-#    if ret_perm:
-#        return shuffles, perm
-#    return shuffle arrays in-place, in the same order, along axis=0
 def parallel_shuffle(arrays, set_seed=-1):
     """
     Parameters:
