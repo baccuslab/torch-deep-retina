69434226fcb3a17e4af8fd4e70e2eb72ea4c92b6
Joshua Melander <melander@stanford.edu>
2019-03-25 13:03:46 -0700

    eltons scripts

--- models/EltonCNN.py
+++ models/EltonCNN.py
@@ -4,35 +4,36 @@ import sys
 sys.path.append('../')
 from models.torch_utils import GaussianNoise
 
-class BNCNN(nn.Module):
+class EltonCNN(nn.Module):
     def __init__(self, n_output_units=5, history = 40, noise_std=0.05):
-        super(BNCNN,self).__init__()
+        super(EltonCNN,self).__init__()
         self.name = 'McNiruNet'
-        self.conv1 = nn.Conv2d(history,8,kernel_size=15)
-        self.batch1 = nn.BatchNorm1d(8*36*36) # keras version uses bnorm with eps=1e-3, momentum=.99
+        self.conv1 = nn.Conv2d(history,8,kernel_size=6)
+        #self.batch1 = nn.BatchNorm1d(8*36*36) # keras version uses bnorm with eps=1e-3, momentum=.99
         self.gaussian1 = GaussianNoise(std=noise_std)
         self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(8,8,kernel_size=11)
-        self.batch2 = nn.BatchNorm1d(8*26*26) # keras version uses bnorm with eps=1e-3, momentum=.99
+        self.conv2 = nn.Conv2d(8,8,kernel_size=6)
+        #self.batch2 = nn.BatchNorm1d(8*26*26) # keras version uses bnorm with eps=1e-3, momentum=.99
         self.gaussian2 = GaussianNoise(std=noise_std)
         self.relu2 = nn.ReLU()
-        self.linear = nn.Linear(8*26*26,n_output_units, bias=False)
-        self.batch3 = nn.BatchNorm1d(n_output_units) # keras version uses bnorm with eps=1e-3, momentum=.99
+        self.linear = nn.Linear(8*10*10,n_output_units, bias=False)
+        #self.batch3 = nn.BatchNorm1d(n_output_units) # keras version uses bnorm with eps=1e-3, momentum=.99
         self.losses = []
         self.actgrad1=[]
         self.actgrad2=[]
         
     def forward(self, x):
         x = self.conv1(x)
-        x = self.batch1(x.view(x.size(0), -1))
-        x = self.gaussian1(x.view(-1, 8, 36, 36))
+        
+       # x = self.batch1(x.view(x.size(0), -1))
+        x = self.gaussian1(x.view(-1, 8, 15, 15))
         x = self.relu1(x)
         x = self.conv2(x)
-        x = self.batch2(x.view(x.size(0), -1))
-        x = self.gaussian2(x.view(-1, 8, 26, 26))
+       # x = self.batch2(x.view(x.size(0), -1))
+        x = self.gaussian2(x.view(-1, 8, 10, 10))
         x = self.relu2(x)
-        x = self.linear(x.view(-1, 8*26*26))
-        x = self.batch3(x)
+        x = self.linear(x.view(-1, 8*10*10))
+        #x = self.batch3(x)
         x = nn.functional.softplus(x)
         return x
     
