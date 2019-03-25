d0ad65ce6b8a76e2885c1be2b34199c0222a0ba4
Satchel Grant <grantsrb@gmail.com>
2019-03-25 10:03:39 -0700

    add working model with weight norm

--- models/Normed_BN_CNN.py
+++ models/Normed_BN_CNN.py
@@ -9,15 +9,18 @@ class NormedBNCNN(nn.Module):
         self.name = 'McNiruNet'
         modules = []
         modules.append(WeightNorm(nn.Conv2d(40,8,kernel_size=15, bias=bias)))
-        modules.append(MeanOnlyBatchNorm((8,36,36), momentum=.99))
+        modules.append(Flatten())
+        modules.append(nn.BatchNorm1d(8*36*36, eps=1e-3, momentum=.99))
         modules.append(GaussianNoise(std=noise))
         modules.append(nn.ReLU())
+        modules.append(Reshape((-1,8,36,36)))
         modules.append(WeightNorm(nn.Conv2d(8,8,kernel_size=11, bias=bias)))
-        modules.append(MeanOnlyBatchNorm((8,26,26), momentum=.99))
+        modules.append(Flatten())
+        modules.append(nn.BatchNorm1d(8*26*26, eps=1e-3, momentum=.99))
         modules.append(GaussianNoise(std=noise))
         modules.append(nn.ReLU())
         modules.append(WeightNorm(nn.Linear(8*26*26,output_units, bias=bias)))
-        modules.append(MeanOnlyBatchNorm(output_units, momentum=.99))
+        modules.append(nn.BatchNorm1d(output_units, momentum=.99))
         modules.append(nn.Softplus())
         self.sequential = nn.Sequential(*modules)
         
