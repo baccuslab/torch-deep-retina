0b94f28144462acf0fd2413d5b7161278dbcb9c3
Satchel Grant <grantsrb@gmail.com>
2019-04-01 20:05:23 -0700

    add abstracted shuffling mechanism

--- utils/miscellaneous.py
+++ utils/miscellaneous.py
@@ -1,5 +1,6 @@
 import numpy as np
 import copy
+import torch
 
 def parallel_shuffle(arrays, set_seed=-1):
     """
@@ -23,3 +24,84 @@ def multi_shuffle(arrays):
             arrays[j][idx:idx+1] = temp
             del temp
     return arrays
+
+class DataObj:
+    def __init__(self, data, idxs):
+        self.data = data
+        self.idxs = idxs
+        self.shape = data.shape
+
+    def __len__(self):
+        return len(self.idxs)
+
+    def __getitem__(self,idxs):
+        return self.data[self.idxs[idxs]]
+
+    def __call__(self,idxs):
+        return self.data[self.idxs[idxs]]
+
+class ShuffledDataSplit:
+    """
+    This class is used to abstract away the permutation indexing required
+    for shuffling large datasets.
+    """
+
+    def __init__(self, data, val_size=30000):
+        """
+        data - a class or named tuple containing an X and y member variable.
+        val_size - the number of samples dedicated to validation
+        """
+        self.X = data.X
+        self.y = data.y
+        if type(self.X) == type(np.array([])):
+            self.perm = np.random.permutation(self.X.shape[0]).astype('int')
+        else:
+            self.perm = torch.randperm(self.X.shape[0]).long()
+        self.train_idxs = self.perm[:-val_size]
+        self.val_idxs = self.perm[-val_size:]
+        self.train_X = DataObj(self.X, self.train_idxs)
+        self.train_y = DataObj(self.y, self.train_idxs)
+        self.val_X = DataObj(self.X, self.val_idxs)
+        self.val_y = DataObj(self.y, self.val_idxs)
+        self.train_shape = (len(self.train_idxs), *self.X.shape[-3:])
+        self.val_shape = (len(self.val_idxs), *self.X.shape[-3:])
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[self.perm[idx]]
+
+    def torch(self):
+        self.X = torch.FloatTensor(self.X)
+        self.y = torch.FloatTensor(self.y)
+        self.perm = torch.LongTensor(self.perm)
+        self.train_idxs = self.perm[:-self.val_shape[0]]
+        self.val_idxs = self.perm[-self.val_shape[0]:]
+        self.train_X = DataObj(self.X, self.train_idxs)
+        self.train_y = DataObj(self.y, self.train_idxs)
+        self.val_X = DataObj(self.X, self.val_idxs)
+        self.val_y = DataObj(self.y, self.val_idxs)
+
+    def numpy(self):
+        self.X = np.asarray(self.X)
+        self.y = np.asarray(self.y)
+        self.perm = np.asarray(self.perm).astype('int')
+        self.train_idxs = self.perm[:-self.val_shape[0]]
+        self.val_idxs = self.perm[-self.val_shape[0]:]
+        self.train_X = DataObj(self.X, self.train_idxs)
+        self.train_y = DataObj(self.y, self.train_idxs)
+        self.val_X = DataObj(self.X, self.val_idxs)
+        self.val_y = DataObj(self.y, self.val_idxs)
+
+    #def train_X(self,idxs):
+    #    return self.X[self.train_idxs[idxs]]
+    #    
+    #def train_y(self,idxs):
+    #    return self.y[self.train_idxs[idxs]]
+
+    #def val_X(self,idxs):
+    #    return self.X[self.val_idxs[idxs]]
+    #    
+    #def val_y(self,idxs):
+    #    return self.y[self.val_idxs[idxs]]
