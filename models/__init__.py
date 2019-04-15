38e6632236884c5858ad97a248dd97d0f8213c1c
Satchel Grant <grantsrb@gmail.com>
2019-04-15 11:45:28 -0700

    add new models to init

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 import models.torch_utils
 from models.CNN import CNN
 from models.BN_CNN import BNCNN
+from models.BN_CNN_2D import BNCNN2D
 from models.SS_CNN import SSCNN
 from models.Dales_BN_CNN import DalesBNCNN
 from models.Dales_SS_CNN import DalesSSCNN
@@ -11,3 +12,4 @@ from models.Normed_BN_CNN import NormedBNCNN
 from models.Skip_BN_CNN import SkipBNCNN
 from models.DalesSkip_BN_CNN import DalesSkipBNCNN
 from models.SkipBN_BN_CNN import SkipBNBNCNN
+from models.Multi_Data_BN_CNN import MultiDataBNCNN
