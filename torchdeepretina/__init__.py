9eeef64c66d12c9be5d9885fd4beae66b7951caf
Satchel Grant <grantsrb@gmail.com>
2019-04-30 08:46:31 -0700

    fix init importing

--- torchdeepretina/__init__.py
+++ torchdeepretina/__init__.py
@@ -1,1 +1,12 @@
 __version__ = '0.1.0'
+import torchdeepretina.batch_compute
+import torchdeepretina.stimuli
+import torchdeepretina.deepretina_loader
+import torchdeepretina.intracellular
+import torchdeepretina.miscellaneous
+import torchdeepretina.physiology
+import torchdeepretina.plotting
+import torchdeepretina.retinal_phenomena
+import torchdeepretina.retio
+import torchdeepretina.torch_utils
+import torchdeepretina.visualizations
