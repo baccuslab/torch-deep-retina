16c0ca89b5fe622b0486f54e401062d476c0be46
Satchel Grant <grantsrb@gmail.com>
2019-06-28 13:39:41 -0700

    change file name

--- torchdeepretina/__init__.py
+++ torchdeepretina/__init__.py
@@ -3,7 +3,7 @@ __version__ = '0.1.0'
 #import torchdeepretina.stimuli
 import torchdeepretina.deepretina_loader
 #import torchdeepretina.intracellular
-import torchdeepretina.miscellaneous
+import torchdeepretina.utils
 #import torchdeepretina.physiology
 #import torchdeepretina.plotting
 #import torchdeepretina.retinal_phenomena
