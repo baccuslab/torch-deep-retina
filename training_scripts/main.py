c82f5983f4c13429d95fc69041b4803d12f05371
Satchel Grant <grantsrb@gmail.com>
2019-07-08 15:46:10 -0700

    make training scripts using training package in tdr

--- training_scripts/main.py
+++ training_scripts/main.py
@@ -2,26 +2,23 @@
 Used as a general training script. Can give command line arguments to specify which json files to use
 for hyperparameters and the ranges that those hyperparameters can take on.
 
-$ python3 general_training.py params=hyperparams.json ranges=hyperranges.json devices=0,1 cuda_buffer=3000 ram_buffer=6000 n_workers=4
+$ python3 general_training.py params=hyperparams.json ranges=hyperranges.json
 
 Defaults to hyperparams.json and hyperranges.json if no arguments are provided
 """
 import sys
+import os
 import time
 import numpy as np
 import torch
-from torchdeepretina.training import mp_hyper_search
+import select
+from torchdeepretina.training import hyper_search
 from torchdeepretina.utils import load_json
-import torch.multiprocessing as mp
 
 if __name__ == "__main__":
-    mp.set_start_method('forkserver')
     hyperparams_file = "hyps/hyperparams.json"
     hyperranges_file = 'hyps/hyperranges.json'
-    visible_devices = {0,1,2,3}
-    cuda_buffer = 3000
-    ram_buffer = 6000
-    n_workers = 2
+    device = 0
     if len(sys.argv) > 1:
         for i,arg in enumerate(sys.argv[1:]):
             temp = sys.argv[1].split("=")
@@ -30,14 +27,6 @@ if __name__ == "__main__":
                     hyperparams_file = temp[1]
                 elif "ranges" == temp[0]:
                     hyperranges_file = temp[1]
-                elif "devices" == temp[0]:
-                    visible_devices = set([int(x) for x in temp[1].split(",")])
-                elif "cuda_buffer" == temp[0]:
-                    cuda_buffer = int(temp[1])
-                elif "ram_buffer" == temp[0]:
-                    ram_buffer = int(temp[1])
-                elif "n_workers" == temp[0]:
-                    n_workers = int(temp[1])
             else:
                 if i == 0 or "hyperparams" in arg:
                     hyperparams_file = arg
@@ -46,10 +35,6 @@ if __name__ == "__main__":
     print()
     print("Using hyperparams file:", hyperparams_file)
     print("Using hyperranges file:", hyperranges_file)
-    print("Visible Devices:", visible_devices)
-    print("CUDA Buffer:", cuda_buffer)
-    print("RAM Buffer:", ram_buffer)
-    print("N Workers:", n_workers)
     print()
 
     hyps = load_json(hyperparams_file)
@@ -98,7 +83,6 @@ if __name__ == "__main__":
 
     keys = list(hyp_ranges.keys())
     start_time = time.time()
-    mp_hyper_search(hyps, hyp_ranges, keys, visible_devices=visible_devices, 
-                          n_workers=n_workers, cuda_buffer=cuda_buffer, ram_buffer=ram_buffer)
+    hyper_search(hyps, hyp_ranges, keys, device)
     print("Total Execution Time:", time.time() - start_time)
 
