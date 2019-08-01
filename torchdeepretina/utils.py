000dfeb1bcab90857c803c9e0ce955f58efce0e1
Satchel Grant <grantsrb@gmail.com>
2019-08-01 13:10:08 -0700

    transfer deepretina_loader into datas

--- torchdeepretina/utils.py
+++ torchdeepretina/utils.py
@@ -10,6 +10,61 @@ def load_json(file_name):
         j = json.loads(s)
     return j
 
+def rolling_window(array, window, time_axis=0):
+    """
+    Make an ndarray with a rolling window of the last dimension
+
+    Parameters
+    ----------
+    array : array_like
+        Array to add rolling window to
+
+    window : int
+        Size of rolling window
+
+    time_axis : int, optional
+        The axis of the temporal dimension, either 0 or -1 (Default: 0)
+
+    Returns
+    -------
+    Array that is a view of the original array with a added dimension
+    of size w.
+
+    Examples
+    --------
+    >>> x=np.arange(10).reshape((2,5))
+    >>> rolling_window(x, 3)
+    array([[[0, 1, 2], [1, 2, 3], [2, 3, 4]],
+           [[5, 6, 7], [6, 7, 8], [7, 8, 9]]])
+
+    Calculate rolling mean of last dimension:
+
+    >>> np.mean(rolling_window(x, 3), -1)
+    array([[ 1.,  2.,  3.],
+           [ 6.,  7.,  8.]])
+    """
+    if time_axis == 0:
+        array = array.T
+
+    elif time_axis == -1:
+        pass
+
+    else:
+        raise ValueError('Time axis must be 0 (first dimension) or -1 (last)')
+
+    assert window >= 1, "`window` must be at least 1."
+    assert window < array.shape[-1], "`window` is too long."
+
+    # with strides
+    shape = array.shape[:-1] + (array.shape[-1] - window, window)
+    strides = array.strides + (array.strides[-1],)
+    arr = np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
+
+    if time_axis == 0:
+        return np.rollaxis(arr.T, 1, 0)
+    else:
+        return arr
+
 def get_cuda_info():
     """
     Get the current gpu usage. 
@@ -88,6 +143,76 @@ def multi_shuffle(arrays):
             del temp
     return arrays
 
+def save_checkpoint(save_dict, folder, exp_id, del_prev=False):
+    """
+    save_dict: dict
+        all things to save to file
+    folder: str
+        path of folder to be saved to
+    exp_id: str
+        additional name to be prepended to path file string
+    del_prev: bool
+        if true, deletes the model_state_dict and optim_state_dict of the save of the
+        previous file (saves space)
+    """
+    if del_prev:
+        prev_path = os.path.join(folder, exp_id + "_epoch_" + str(save_dict['epoch']-1) + '.pth')
+        if os.path.exists(prev_path):
+            device = torch.device("cpu")
+            data = torch.load(prev_path, map_location=device)
+            keys = list(data.keys())
+            for key in keys:
+                if "state_dict" in key:
+                    del data[key]
+            torch.save(data, prev_path)
+        elif save_dict['epoch'] != 0:
+            print("Failed to find previous checkpoint", prev_path)
+    path = os.path.join(folder, exp_id + '_epoch_' + str(save_dict['epoch'])) + '.pth'
+    path = os.path.abspath(os.path.expanduser(path))
+    torch.save(save_dict, path)
+
+def load_checkpoint(checkpt_path):
+    """
+    Can load a specific model file both architecture and state_dict if the file 
+    contains a model_state_dict key, or can just load the architecture.
+
+    checkpt_path: str
+        path to checkpoint file
+    """
+    data = torch.load(folder, map_location=torch.device("cpu"))
+    try:
+        model = globals()[data['model_type']](**data['model_hyps'])
+    except Exception as e:
+        print(e)
+        print("Likely the checkpoint you are using is deprecated. Try using analysis.load_model()")
+    try:
+        model.load_state_dict(data['model_state_dict'])
+    except KeyError as e:
+        print("Failed to load state_dict. This checkpoint does not contain a model state_dict!")
+    return model
+
+def stack_filters(base_filt, stack_filt):
+    """
+    Combines two convolutional filters in a mathematically equal way to performing
+    the convolutions one after the other. Forgive the quadruple for-loop... There's
+    probably a way to parallelize but this was much easier to implement.
+
+    base_filt - torch FloatTensor (Q, R, K1, K1)
+        the first filter in the conv sequence.
+    stack_filt - torch FloatTensor (S, Q, K2, K2)
+        the filter following base_filt in the conv sequence.
+    """
+    kb = base_filt.shape[-1]
+    ks = stack_filt.shape[-1]
+    new_filt = torch.zeros(stack_filt.shape[0], base_filt.shape[1], base_filt.shape[2]+(ks-1), base_filt.shape[3]+(ks-1))
+    new_filt = new_filt.to(base_filt.get_device())
+    for out_chan in range(stack_filt.shape[0]):
+        for in_chan in range(stack_filt.shape[1]): # same as out_chan in base_filt/new_filt
+            for row in range(stack_filt.shape[2]):
+                for col in range(stack_filt.shape[3]):
+                    new_filt[out_chan:out_chan+1, :, row:row+kb, col:col+kb] += base_filt[in_chan]*stack_filt[out_chan, in_chan, row, col]
+    return new_filt
+
 def conv_backwards(z, filt, xshape):
     """
     Used for gradient calculations specific to a single convolutional filter.
@@ -136,7 +261,7 @@ class ShuffledDataSplit:
     for shuffling large datasets.
     """
 
-    def __init__(self, data, val_size=30000, batch_size=512, shuffle=True):
+    def __init__(self, data, val_size=30000, batch_size=512, seq_len=1, shuffle=True):
         """
         data - a class or named tuple containing an X and y member variable.
         val_size - the number of samples dedicated to validation
@@ -157,6 +282,9 @@ class ShuffledDataSplit:
                 self.perm = torch.arange(self.x.shape[0]).long()
         self.train_idxs = self.perm[:-val_size]
         self.val_idxs = self.perm[-val_size:]
+        if seq_len > 1:
+            self.X = rolling_window(self.X, seq_len, time_axis=0)
+            self.y = rolling_window(self.y, seq_len, time_axis=0)
         self.train_X = DataObj(self.X, self.train_idxs)
         self.train_y = DataObj(self.y, self.train_idxs)
         self.val_X = DataObj(self.X, self.val_idxs)
