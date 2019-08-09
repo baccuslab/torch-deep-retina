aef6b12dbbbf5d8537ad233f5cd072b35e8d47da
Satchel Grant <grantsrb@gmail.com>
2019-08-08 20:37:08 -0700

    remove unnecessary files

--- training_scripts/bash_automation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 search_analysis.py linstackfiltsearch/
-
-#for VARIABLE in linearstacked linstack
-#do
-#    python3 general_training.py ${VARIABLE}_hyperparams.json ${VARIABLE}_hyperranges.json
-#    python3 search_analysis.py ${VARIABLE}/
-#done
