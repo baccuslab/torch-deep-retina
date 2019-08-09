310ec677f7351626bfd03be740a2fb1aa936bf3b
Satchel Grant <grantsrb@gmail.com>
2019-08-08 20:08:50 -0700

    remove deprecated code

---
+++ training_scripts/bash_automation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 search_analysis.py linstackfiltsearch/
+
+#for VARIABLE in linearstacked linstack
+#do
+#    python3 general_training.py ${VARIABLE}_hyperparams.json ${VARIABLE}_hyperranges.json
+#    python3 search_analysis.py ${VARIABLE}/
+#done
