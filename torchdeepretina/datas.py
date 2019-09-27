0e1cf3e7b041f2445e724eb481c3f0c2a3a6d5c4
Satchel Grant <grantsrb@gmail.com>
2019-09-27 11:59:59 -0700

    attempt to consolidate duplicate functions

--- torchdeepretina/datas.py
+++ torchdeepretina/datas.py
@@ -9,6 +9,7 @@ from os.path import join, expanduser
 from scipy.stats import zscore
 import pyret.filtertools as ft
 import torch
+import torchdeepretina.stimuli as tdrstimuli
 
 NUM_BLOCKS = {
     '15-10-07': 6,
@@ -155,58 +156,9 @@ def stimcut(data, expt, ci, width=11):
 
 def rolling_window(array, window, time_axis=0):
     """
-    Make an ndarray with a rolling window of the last dimension
-
-    Parameters
-    ----------
-    array : array_like
-        Array to add rolling window to
-
-    window : int
-        Size of rolling window
-
-    time_axis : int, optional
-        The axis of the temporal dimension, either 0 or -1 (Default: 0)
-
-    Returns
-    -------
-    Array that is a view of the original array with a added dimension
-    of size w.
-
-    Examples
-    --------
-    >>> x=np.arange(10).reshape((2,5))
-    >>> rolling_window(x, 3)
-    array([[[0, 1, 2], [1, 2, 3], [2, 3, 4]],
-           [[5, 6, 7], [6, 7, 8], [7, 8, 9]]])
-
-    Calculate rolling mean of last dimension:
-
-    >>> np.mean(rolling_window(x, 3), -1)
-    array([[ 1.,  2.,  3.],
-           [ 6.,  7.,  8.]])
+    See stimuli.py package for details
     """
-    if time_axis == 0:
-        array = array.T
-
-    elif time_axis == -1:
-        pass
-
-    else:
-        raise ValueError('Time axis must be 0 (first dimension) or -1 (last)')
-
-    assert window >= 1, "`window` must be at least 1."
-    assert window < array.shape[-1], "`window` is too long."
-
-    # with strides
-    shape = array.shape[:-1] + (array.shape[-1] - window, window)
-    strides = array.strides + (array.strides[-1],)
-    arr = np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
-
-    if time_axis == 0:
-        return np.rollaxis(arr.T, 1, 0)
-    else:
-        return arr
+    return tdrstimuli.rolling_window(array, window, time_axis)
 
 class DataObj:
     def __init__(self, data, idxs):
