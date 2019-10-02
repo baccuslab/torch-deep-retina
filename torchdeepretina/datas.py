b5b5e2eaed6cc6fb23963a234fb47e61afe6e744
Satchel Grant <grantsrb@gmail.com>
2019-10-01 19:17:30 -0700

    fix load intracellular function

--- torchdeepretina/datas.py
+++ torchdeepretina/datas.py
@@ -10,7 +10,7 @@ from scipy.stats import zscore
 import pyret.filtertools as ft
 import torch
 import torchdeepretina.stimuli as tdrstimuli
-import os
+import torchdeepretina.intracellular as tdrintra
 
 NUM_BLOCKS = {
     '15-10-07': 6,
@@ -152,29 +152,7 @@ def load_interneuron_data(root_path, files=None, filter_length=40, stim_keys={"b
         keys are the cell files, vals are a list of nd array membrane potential responses for each 
         cell within the file
     """
-    if files is None:
-        files = ['bipolars_late_2012.h5', 'bipolars_early_2012.h5', 'amacrines_early_2012.h5', 
-                'amacrines_late_2012.h5', 'horizontals_early_2012.h5', 'horizontals_late_2012.h5']
-    files = [os.path.expanduser(os.path.join(root_path, name)) for name in files]
-    file_ids = []
-    for f in files:
-        file_ids.append(re.split('_|\.', f)[0])
-    num_pots = []
-    stims = dict()
-    mem_pots = dict()
-    for fi in files:
-        stims[fi] = dict()
-        mem_pots[fi] = dict()
-        with h5py.File(fi, 'r') as f:
-            for k in f.keys():
-                if k in stim_keys:
-                    try:
-                        stims[fi][k] = prepare_stim(np.asarray(f[k+'/stimuli']), k)
-                        mem_pots[fi][k] = np.asarray(f[k]['detrended_membrane_potential'])[:, filter_length:]
-                    except Exception as e:
-                        print(e)
-                        print("stim error at", k)
-    return stims, mem_pots, files
+    return tdrintra.load_interneuron_data(root_path=root_path, files=files, filter_length=filter_length, stim_keys=stim_keys)
 
 def stimcut(data, expt, ci, width=11):
     """Cuts out a stimulus around the whitenoise receptive field"""
