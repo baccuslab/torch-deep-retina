e26fe02274c86029b6411ac83cbf80215482462b
Satchel Grant <grantsrb@gmail.com>
2019-10-10 17:02:32 -0700

    update tutorial example, potentially still deprecated

--- tutorials/inference_example.py
+++ tutorials/inference_example.py
@@ -39,13 +39,14 @@ if __name__=="__main__":
     stim_type = checkpt["stim_type"]
     temporal_depth = checkpt['img_shape'][0] # Number of movie frames seen in 1 datapoint
     mean, std = checkpt['norm_stats']['mean'], checkpt['norm_stats']['std']
-    norm_stats = [mean, std] # Used to z-score the test data using the same statistics as the training data
-    test_data = loadexpt(dataset, cells, stim_type, 'test', temporal_depth, 0, norm_stats=norm_stats, 
-                                                                                data_path=path_to_data)
+    norm_stats = [mean, std] # to z-score the test data with statistics of the training data
+    test_data = loadexpt(dataset, cells, stim_type, 'test', temporal_depth, nskip=0, 
+                                        norm_stats=norm_stats, data_path=path_to_data)
 
     # Compute model responses and determine pearson correlation with ganglion cell output
     batch_size = 500
-    model_response = batch_compute_model_response(test_data.X, model, batch_size, recurrent=model.recurrent)
+    model_response = batch_compute_model_response(test_data.X, model, batch_size, 
+                                                        recurrent=model.recurrent)
     print(model_response['output'])
     pearsons = [scipy.stats.pearsonr(model_response['output'][:, i], test_data.y[:, i])[0] 
                                                     for i in range(test_data.y.shape[-1])]
