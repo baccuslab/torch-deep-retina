3c923baf1c61aae982d89fc9c0caa718f90871b5
Xuehao Ding <xhding@stanford.edu>
2019-10-24 19:26:01 -0700

    m

---
+++ xhding/evaluation.py
@@ -0,0 +1,23 @@
+import torch
+import numpy as np
+from scipy.stats import pearsonr
+from train import get_hs
+
+def pearsonr(model, data, n_units, device):
+    hs = get_hs(model, 1, device)
+    model = model.to(device)
+    model.eval()
+    with torch.no_grad():
+        pearsons = []
+        val_pred = []
+        val_targ = []
+        for x,y in data:
+            x = x.to(device)
+            out, hs = model(x, hs)
+            val_pred.append(out.detach().cpu().numpy().squeeze())
+            val_targ.append(y.detach().numpy().squeeze())
+        val_pred = np.stack(val_pred, axis=0)
+        val_targ = np.stack(val_targ, axis=0)
+        for cell in range(n_units):
+            pearsons.append(pearsonr(val_pred[:,cell],val_targ[:,cell])[0])
+        return np.array(pearsons).mean()
--- xhding/train.py
+++ xhding/train.py
@@ -6,13 +6,15 @@ import numpy as np
 from collections import deque
 from models import *
 from data import *
+from evaluation import pearsonr
 
-def get_hs(model, batch_size):
-    hs[0] = torch.zeros(batch_size, *model.h_shapes[0])
+def get_hs(model, batch_size, device):
+    hs = []
+    hs.append(torch.zeros(batch_size, *model.h_shapes[0]).to(device))
     hs[0][:,0] = 1
-    hs[1] = deque([],maxlen=model.seq_len)
+    hs.append(deque([],maxlen=model.seq_len))
     for i in range(model.seq_len):
-        hs[1].append(torch.zeros(batch_size, *model.h_shapes[1]))
+        hs[1].append(torch.zeros(batch_size, *model.h_shapes[1]).to(device))
     return hs
 
 def train(cfg):
@@ -41,7 +43,7 @@ def train(cfg):
     
     for epoch in range(cfg.epoch):
         epoch_loss = 0
-        hs = get_hs(model, cfg.Data.batch_size).to(device)
+        hs = get_hs(model, cfg.Data.batch_size, device)
         for idx,(x,y) in enumerate(train_data):
             loss = 0
             x = x.to(device)
@@ -54,13 +56,15 @@ def train(cfg):
                 epoch_loss += loss.detach().cpu().numpy()
                 loss = 0
                 hs = hs.detach()
+                hs[0] = hs[0].data.clone()
+                hs[1] = deque([h.data.clone() for h in hs[1]], maxlen=model.seq_len)
                 
         epoch_loss = epoch_loss / len(train_dataset) * cfg.Data.batch_size
         
-        model.eval()
+        validation_data =  DataLoader(dataset=ValidationDataset(cfg.Data.data_path))
+        pearson = pearsonr(model, validation_data, cfg.Model.n_units, device)
         
-        
-        print('epoch: {}, loss: {}'.format(epoch, epoch_loss))
+        print('epoch: {}, loss: {}, pearson correlation: {}'.format(epoch, epoch_loss, pearson))
         
         if epoch%cfg.save_intrvl == 0:
             try:
@@ -69,7 +73,8 @@ def train(cfg):
             except FileExistsError:
                 print("Directory already exists")
             save_path = os.path.join(cfg.save_path, cfg.exp_id, 
-                                     'epoch_{}_loss_{}'.format(epoch, epoch_loss))
+                                     'epoch_{}_loss_{}_pearson_{}'
+                                     .format(epoch, epoch_loss, pearson)+'.pth')
             torch.save(model.state_dict(), save_path)
     
     
