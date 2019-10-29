4fa834eef2f5a55136bbb99a5b662b05a9a599d7
Satchel Grant <grantsrb@gmail.com>
2019-10-29 11:02:44 -0700

    refactor

--- torchdeepretina/models.py
+++ torchdeepretina/models.py
@@ -14,9 +14,12 @@ def try_kwarg(kwargs, key, default):
         return default
 
 class TDRModel(nn.Module):
-    def __init__(self, n_units=5, noise=.05, bias=True, linear_bias=None, chans=[8,8], bn_moment=.01, 
-                                 softplus=True, inference_exp=False, img_shape=(40,50,50), ksizes=(15,11), 
-                                 recurrent=False, kinetic=False, convgc=False, centers=None, **kwargs):
+    def __init__(self, n_units=5, noise=.05, bias=True, linear_bias=None, chans=[8,8],
+                                                bn_moment=.01, softplus=True, 
+                                                inference_exp=False, img_shape=(40,50,50), 
+                                                ksizes=(15,11), recurrent=False, 
+                                                kinetic=False, convgc=False, 
+                                                centers=None, bnorm_d=1, **kwargs):
         super().__init__()
         self.n_units = n_units
         self.chans = chans 
@@ -32,23 +35,23 @@ class TDRModel(nn.Module):
         self.kinetic = kinetic
         self.convgc = convgc
         self.centers = centers
+        assert bnorm_d == 1 or bnorm_d == 2,\
+                                "Only 1 and 2 dimensional batchnorm are currently supported"
+        self.bnorm_d = bnorm_d
     
     def forward(self, x):
         return x
 
     def extra_repr(self):
         try:
-            return 'n_units={}, noise={}, bias={}, linear_bias={}, chans={}, bn_moment={}, softplus={}, inference_exp={}, img_shape={}, ksizes={}'.format(self.n_units, self.noise, self.bias, self.linear_bias, self.chans, self.bn_moment, self.softplus, self.inference_exp, self.img_shape, self.ksizes)
+            s = 'n_units={}, noise={}, bias={}, linear_bias={}, chans={}, bn_moment={}, '+\
+                                    'softplus={}, inference_exp={}, img_shape={}, ksizes={}'
+            return s.format(self.n_units, self.noise, self.bias, self.linear_bias,
+                                        self.chans, self.bn_moment, self.softplus,
+                                        self.inference_exp, self.img_shape, self.ksizes)
         except:
             pass
     
-    def detach(self):
-        for p in self.parameters():
-            try:
-                p.detach()
-            except:
-                pass
-
     def requires_grad(self, state):
         for p in self.parameters():
             try:
@@ -64,22 +67,34 @@ class BNCNN(TDRModel):
         modules = []
         self.shapes = []
         shape = self.img_shape[1:]
-        modules.append(nn.Conv2d(self.img_shape[0],self.chans[0],kernel_size=self.ksizes[0], bias=self.bias))
+        modules.append(nn.Conv2d(self.img_shape[0],self.chans[0],kernel_size=self.ksizes[0], 
+                                                                            bias=self.bias))
         shape = update_shape(shape, self.ksizes[0])
         self.shapes.append(tuple(shape))
-        modules.append(Flatten())
-        modules.append(nn.BatchNorm1d(self.chans[0]*shape[0]*shape[1], eps=1e-3, momentum=self.bn_moment))
+        if self.bnorm_d == 1:
+            modules.append(Flatten())
+            modules.append(nn.BatchNorm1d(self.chans[0]*shape[0]*shape[1], eps=1e-3,
+                                                           momentum=self.bn_moment))
+            modules.append(Reshape((-1,self.chans[0],*shape)))
+        else:
+            modules.append(nn.BatchNorm2d(self.chans[0], eps=1e-3, momentum=self.bn_moment))
         modules.append(GaussianNoise(std=self.noise))
         modules.append(nn.ReLU())
-        modules.append(Reshape((-1,self.chans[0],*shape)))
-        modules.append(nn.Conv2d(self.chans[0],self.chans[1],kernel_size=self.ksizes[1], bias=self.bias))
+        modules.append(nn.Conv2d(self.chans[0],self.chans[1],kernel_size=self.ksizes[1], 
+                                                                        bias=self.bias))
         shape = update_shape(shape, self.ksizes[1])
         self.shapes.append(tuple(shape))
-        modules.append(Flatten())
-        modules.append(nn.BatchNorm1d(self.chans[1]*shape[0]*shape[1], eps=1e-3, momentum=self.bn_moment))
+        if self.bnorm_d == 1:
+            modules.append(Flatten())
+            modules.append(nn.BatchNorm1d(self.chans[1]*shape[0]*shape[1], eps=1e-3, 
+                                                            momentum=self.bn_moment))
+        else:
+            modules.append(nn.BatchNorm2d(self.chans[1], eps=1e-3, momentum=self.bn_moment))
+            modules.append(Flatten())
         modules.append(GaussianNoise(std=self.noise))
         modules.append(nn.ReLU())
-        modules.append(nn.Linear(self.chans[1]*shape[0]*shape[1],self.n_units, bias=self.linear_bias))
+        modules.append(nn.Linear(self.chans[1]*shape[0]*shape[1],self.n_units,
+                                                       bias=self.linear_bias))
         modules.append(nn.BatchNorm1d(self.n_units, eps=1e-3, momentum=self.bn_moment))
         if self.softplus:
             modules.append(nn.Softplus())
@@ -107,6 +122,124 @@ class BNCNN(TDRModel):
         if not self.training and self.infr_exp:
             return torch.exp(self.sequential(x))
         return self.sequential(x)
+
+class LinearStackedBNCNN(TDRModel):
+    def __init__(self, drop_p=0, one2one=False, stack_ksizes=[3,3], stack_chans=[None,None],
+                                                 final_bias=False, paddings=None, **kwargs):
+        super().__init__(**kwargs)
+        self.name = 'StackedNet'
+        self.drop_p = drop_p
+        self.one2one = one2one
+        self.stack_ksizes = stack_ksizes
+        self.stack_chans = stack_chans
+        self.paddings = [0 for x in stack_ksizes] if paddings is None else paddings
+        self.final_bias = final_bias
+        shape = self.img_shape[1:] # (H, W)
+        self.shapes = []
+        modules = []
+
+        ##### First Layer
+        # Convolution
+        if one2one:
+            modules.append(OneToOneLinearStackedConv2d(self.img_shape[0],self.chans[0],
+                                                            kernel_size=self.ksizes[0], 
+                                                            padding=self.paddings[0],
+                                                            bias=self.bias))
+        else:
+            modules.append(LinearStackedConv2d(self.img_shape[0],self.chans[0],
+                                                    kernel_size=self.ksizes[0], 
+                                                    abs_bnorm=False, bias=self.bias, 
+                                                    stack_chan=self.stack_chans[0], 
+                                                    stack_ksize=self.stack_ksizes[0],
+                                                    drop_p=self.drop_p, 
+                                                    padding=self.paddings[0]))
+        shape = update_shape(shape, self.ksizes[0], padding=self.paddings[0])
+        self.shapes.append(tuple(shape))
+        # BatchNorm
+        if self.bnorm_d == 1:
+            modules.append(Flatten())
+            modules.append(AbsBatchNorm1d(self.chans[0]*shape[0]*shape[1], eps=1e-3, 
+                                                            momentum=self.bn_moment))
+            modules.append(Reshape((-1,self.chans[0],shape[0], shape[1])))
+        else:
+            modules.append(AbsBatchNorm2d(self.chans[0], eps=1e-3, momentum=self.bn_moment))
+        # Noise and ReLU
+        modules.append(GaussianNoise(std=self.noise))
+        modules.append(nn.ReLU())
+
+        ##### Second Layer
+        # Convolution
+        if one2one:
+            modules.append(OneToOneLinearStackedConv2d(self.chans[0],self.chans[1],
+                                                        kernel_size=self.ksizes[1], 
+                                                        padding=self.paddings[1],
+                                                        bias=self.bias))
+        else:
+            modules.append(LinearStackedConv2d(self.chans[0],self.chans[1],
+                                                    kernel_size=self.ksizes[1], 
+                                                    abs_bnorm=False, bias=self.bias, 
+                                                    stack_chan=self.stack_chans[1], 
+                                                    stack_ksize=self.stack_ksizes[1],
+                                                    padding=self.paddings[1],
+                                                    drop_p=self.drop_p))
+        shape = update_shape(shape, self.ksizes[1], padding=self.paddings[1])
+        self.shapes.append(tuple(shape))
+        # BatchNorm
+        if self.bnorm_d == 1:
+            modules.append(Flatten())
+            modules.append(AbsBatchNorm1d(self.chans[1]*shape[0]*shape[1], eps=1e-3, 
+                                                        momentum=self.bn_moment))
+        else:
+            modules.append(AbsBatchNorm2d(self.chans[1], eps=1e-3, momentum=self.bn_moment))
+            modules.append(Flatten())
+        # Noise and ReLU
+        modules.append(GaussianNoise(std=self.noise))
+        modules.append(nn.ReLU())
+
+        ##### Final Layer
+        if self.convgc:
+            modules.append(Reshape((-1, self.chans[1], shape[0], shape[1])))
+            modules.append(nn.Conv2d(self.chans[1],self.n_units,kernel_size=self.ksizes[2], 
+                                                                    bias=self.linear_bias))
+            shape = update_shape(shape, self.ksizes[2])
+            self.shapes.append(tuple(shape))
+            modules.append(GrabUnits(self.centers, self.ksizes, self.img_shape, self.n_units))
+            modules.append(AbsBatchNorm1d(self.n_units, momentum=self.bn_moment))
+        else:
+            modules.append(nn.Linear(self.chans[1]*shape[0]*shape[1], self.n_units, 
+                                                                bias=self.linear_bias))
+            modules.append(AbsBatchNorm1d(self.n_units, eps=1e-3, momentum=self.bn_moment))
+        if self.softplus:
+            modules.append(nn.Softplus())
+        else:
+            modules.append(Exponential(train_off=True))
+        if self.final_bias:
+            modules.append(Add(0,trainable=True))
+
+        self.sequential = nn.Sequential(*modules)
+
+    def forward(self, x):
+        if not self.training and self.infr_exp:
+            return torch.exp(self.sequential(x))
+        return self.sequential(x)
+    
+    def tiled_forward(self,x):
+        """
+        Allows for the fully convolutional functionality
+        """
+        if not self.convgc:
+            return self.forward(x)
+        fx = self.sequential[:-3](x) # Remove GrabUnits layer
+        bnorm = self.sequential[-2]
+        # Perform 2d batchnorm using 1d parameters collected from training
+        fx = torch.nn.functional.batch_norm(fx, bnorm.running_mean.data, bnorm.running_var.data,
+                                                    weight=bnorm.scale.abs(), bias=bnorm.shift, 
+                                                    eps=bnorm.eps, momentum=bnorm.momentum, 
+                                                    training=self.training)
+        fx = self.sequential[-1](fx)
+        if not self.training and self.infr_exp:
+            return torch.exp(fx)
+        return fx
 
 class LN(TDRModel):
     def __init__(self, drop_p=0, **kwargs):
@@ -380,106 +513,6 @@ class RNNCNN(TDRModel):
         if not self.training and self.infr_exp:
             fx = torch.exp(fx)
         return fx, [h1, h2]
-
-class LinearStackedBNCNN(TDRModel):
-    def __init__(self, drop_p=0, one2one=False, stack_ksizes=[3,3], stack_chans=[None,None],
-                                                                final_bias=False, 
-                                                                paddings=None, **kwargs):
-        super().__init__(**kwargs)
-        self.name = 'StackedNet'
-        self.drop_p = drop_p
-        self.one2one = one2one
-        self.stack_ksizes = stack_ksizes
-        self.stack_chans = stack_chans
-        self.paddings = [0 for x in stack_ksizes] if paddings is None else paddings
-        self.final_bias = final_bias
-        shape = self.img_shape[1:] # (H, W)
-        self.shapes = []
-
-        modules = []
-        if one2one:
-            modules.append(OneToOneLinearStackedConv2d(self.img_shape[0],self.chans[0],
-                                                            kernel_size=self.ksizes[0], 
-                                                            padding=self.paddings[0],
-                                                            bias=self.bias))
-        else:
-            modules.append(LinearStackedConv2d(self.img_shape[0],self.chans[0],
-                                                    kernel_size=self.ksizes[0], 
-                                                    abs_bnorm=False, bias=self.bias, 
-                                                    stack_chan=self.stack_chans[0], 
-                                                    stack_ksize=self.stack_ksizes[0],
-                                                    drop_p=self.drop_p, 
-                                                    padding=self.paddings[0]))
-        shape = update_shape(shape, self.ksizes[0], padding=self.paddings[0])
-        self.shapes.append(tuple(shape))
-        modules.append(Flatten())
-        modules.append(AbsBatchNorm1d(self.chans[0]*shape[0]*shape[1], eps=1e-3, 
-                                                        momentum=self.bn_moment))
-        modules.append(GaussianNoise(std=self.noise))
-        modules.append(nn.ReLU())
-        modules.append(Reshape((-1,self.chans[0],shape[0], shape[1])))
-        if one2one:
-            modules.append(OneToOneLinearStackedConv2d(self.chans[0],self.chans[1],
-                                                        kernel_size=self.ksizes[1], 
-                                                        padding=self.paddings[1],
-                                                        bias=self.bias))
-        else:
-            modules.append(LinearStackedConv2d(self.chans[0],self.chans[1],
-                                                    kernel_size=self.ksizes[1], 
-                                                    abs_bnorm=False, bias=self.bias, 
-                                                    stack_chan=self.stack_chans[1], 
-                                                    stack_ksize=self.stack_ksizes[1],
-                                                    padding=self.paddings[1],
-                                                    drop_p=self.drop_p))
-        shape = update_shape(shape, self.ksizes[1], padding=self.paddings[1])
-        self.shapes.append(tuple(shape))
-        modules.append(Flatten())
-        modules.append(AbsBatchNorm1d(self.chans[1]*shape[0]*shape[1], eps=1e-3, 
-                                                        momentum=self.bn_moment))
-        modules.append(GaussianNoise(std=self.noise))
-        modules.append(nn.ReLU())
-        if self.convgc:
-            modules.append(Reshape((-1, self.chans[1], shape[0], shape[1])))
-            modules.append(nn.Conv2d(self.chans[1],self.n_units,kernel_size=self.ksizes[2], 
-                                                                    bias=self.linear_bias))
-            shape = update_shape(shape, self.ksizes[2])
-            self.shapes.append(tuple(shape))
-            modules.append(GrabUnits(self.centers, self.ksizes, self.img_shape, self.n_units))
-            modules.append(AbsBatchNorm1d(self.n_units, momentum=self.bn_moment))
-        else:
-            modules.append(nn.Linear(self.chans[1]*shape[0]*shape[1], self.n_units, 
-                                                                bias=self.linear_bias))
-            modules.append(AbsBatchNorm1d(self.n_units, eps=1e-3, momentum=self.bn_moment))
-        if self.softplus:
-            modules.append(nn.Softplus())
-        else:
-            modules.append(Exponential(train_off=True))
-        if self.final_bias:
-            modules.append(Add(0,trainable=True))
-        self.sequential = nn.Sequential(*modules)
-
-    def forward(self, x):
-        if not self.training and self.infr_exp:
-            return torch.exp(self.sequential(x))
-        return self.sequential(x)
-    
-    def tiled_forward(self,x):
-        """
-        Allows for the fully convolutional functionality
-        """
-        if not self.convgc:
-            return self.forward(x)
-        fx = self.sequential[:-3](x) # Remove GrabUnits layer
-        bnorm = self.sequential[-2]
-        # Perform 2d batchnorm using 1d parameters
-        fx = torch.nn.functional.batch_norm(fx, bnorm.running_mean.data, bnorm.running_var.data,
-                                            weight=bnorm.scale.abs(), bias=bnorm.shift, 
-                                            eps=bnorm.eps, momentum=bnorm.momentum, 
-                                            training=self.training)
-        fx = self.sequential[-1](fx)
-        if not self.training and self.infr_exp:
-            return torch.exp(fx)
-        return fx
 
 class KineticsModel(TDRModel):
     def __init__(self, bnorm=True, drop_p=0, scale_kinet=False, recur_seq_len=5, **kwargs):
