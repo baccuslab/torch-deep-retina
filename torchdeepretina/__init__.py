f7edc642d94494933d0ba4f9c70d01fa4f038a18
Saetch Mo <grantsrb@gmail.com>
2020-01-24 14:55:27 -0800

    remove residual compression training

--- torchdeepretina/__init__.py
+++ torchdeepretina/__init__.py
@@ -1,5 +1,4 @@
 __version__ = '0.1.0'
 from . import stimuli, datas, intracellular, utils, retinal_phenomena, custom_modules, models, analysis, training
 from .analysis import *
-from . import compression_training
 
