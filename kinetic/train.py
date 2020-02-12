585350301ebb8f5ef47a05f0baeb683edcc29380
Xuehao Ding <xhding@stanford.edu>
2020-02-12 18:11:48 -0500

    m

--- kinetic/train.py
+++ kinetic/train.py
@@ -14,6 +14,12 @@ from kinetic.config import get_default_cfg, get_custom_cfg
 
 def train(cfg):
     
+    if not os.path.exists(os.path.join(cfg.save_path, cfg.exp_id)):
+        os.mkdir(os.path.join(cfg.save_path, cfg.exp_id))
+        
+    with open(os.path.join(cfg.save_path, cfg.exp_id, 'cfg'), 'w') as f:
+        f.write(str(cfg))
+    
     device = torch.device('cuda:'+str(cfg.gpu))
     
     model = select_model(cfg, device)
@@ -29,13 +35,14 @@ def train(cfg):
     if cfg.Model.checkpoint != '':
         checkpoint = torch.load(cfg.Model.checkpoint, map_location=device)
         model.load_state_dict(checkpoint['model_state_dict'])
-        start_epoch = checkpoint['epoch']
+        start_epoch = checkpoint['epoch'] + 1
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     
     train_dataset = TrainDataset(cfg)
     batch_sampler = BatchRnnSampler(length=len(train_dataset), batch_size=cfg.Data.batch_size,
                                     seq_len=cfg.Data.trunc_int)
     train_data = DataLoader(dataset=train_dataset, batch_sampler=batch_sampler)
+    validation_data =  DataLoader(dataset=ValidationDataset(cfg))
     
     for epoch in range(start_epoch, start_epoch + cfg.epoch):
         epoch_loss = 0
@@ -61,19 +68,14 @@ def train(cfg):
                 
         epoch_loss = epoch_loss / len(train_dataset) * cfg.Data.batch_size
         
-        validation_data =  DataLoader(dataset=ValidationDataset(cfg))
         pearson = pearsonr_eval(model, validation_data, cfg.Model.n_units, 600, device)
         
-        print('epoch: {}, loss: {}, pearson correlation: {}'.format(epoch, epoch_loss, pearson))
+        print('epoch: {:03d}, loss: {:.2f}, pearson correlation: {:.4f}'.format(epoch, epoch_loss, pearson))
         
         if epoch%cfg.save_intvl == 0:
-            try:
-                os.mkdir(os.path.join(cfg.save_path, cfg.exp_id))
-                print("Directory Created ") 
-            except FileExistsError:
-                pass
+
             save_path = os.path.join(cfg.save_path, cfg.exp_id, 
-                                     'epoch_{}_loss_{}_pearson_{}'
+                                     'epoch_{:03d}_loss_{:.2f}_pearson_{:.4f}'
                                      .format(epoch, epoch_loss, pearson)+'.pth')
 
             torch.save({'epoch': epoch,
