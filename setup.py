f76643abd172f5222b3b49a00d1ffda45b42ecf5
Satchel Grant <grantsrb@stanford.edu>
2020-03-05 17:32:56 -0500

    automatically install mplstyle

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+import shutil
+import atexit
+
+import matplotlib
+
+def install_mplstyle():
+    stylefile = "deepretina.mplstyle"
+
+    mpl_stylelib_dir = os.path.join(matplotlib.get_configdir() ,"stylelib")
+    if not os.path.exists(mpl_stylelib_dir):
+        os.makedirs(mpl_stylelib_dir)
+
+    print("Installing style into", mpl_stylelib_dir)
+    shutil.copy(
+        os.path.join(os.path.dirname(__file__), stylefile),
+        os.path.join(mpl_stylelib_dir, stylefile))
+
+class PostInstallMoveFile(install):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        atexit.register(install_mplstyle)
 
 setup(name='torchdeepretina',
       packages=find_packages(),
