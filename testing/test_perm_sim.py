0a8f8cd07ba0508ff1b04d335472b1d8d0921be6
Satchel Grant <grantsrb@stanford.edu>
2020-06-16 03:52:07 -0400

    update testing

--- testing/test_perm_sim.py
+++ testing/test_perm_sim.py
@@ -4,16 +4,29 @@ import torchdeepretina as tdr
 
 
 if __name__=="__main__":
-    N = 100
+    N = 4
     T = 10000
+    S = 5
 
     x = np.random.randn(T,N)
-    x = x*(10**np.arange(N))
-
+    #x = x*(10**np.arange(N))
     perm = np.random.permutation(N).astype(np.int)
     y = x[:,perm]
+    sim = tdr.utils.perm_similarity(x, y, grad_fit=False, patience=10,
+                                                        vary_space=False)
+    print("sim1:", sim)
+
+    print("perm",perm)
+    y = np.random.random((T,N,S))
+    ss = np.random.randint(0,S,N)
+    print("ss:", ss)
+    for i in range(N):
+        s = ss[i]
+        y[:,i,s] = x[:,perm[i]]
+
     #y = np.concatenate([x[:,perm],np.random.random((T,10))],axis=-1)
-    sim = tdr.utils.perm_similarity(y, x, grad_fit=False, patience=10)
+    sim = tdr.utils.perm_similarity(x, y, grad_fit=False, patience=10,
+                                                        vary_space=True)
     print(sim)
     
     
