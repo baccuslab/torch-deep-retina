42cde1ebcace5cc0c793bac0b0e1ad579684d6db
i-aji <51084070+i-aji@users.noreply.github.com>
2024-02-22 14:55:01 +0100

    change module name from jetpack to jetplot

--- figs/deep-retina-figures/f2_response.py
+++ figs/deep-retina-figures/f2_response.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fftfreq, fft
 from tqdm import tqdm
-from jetpack import errorplot
+from jetplot import errorplot
 import torchdeepretina as tdr
 import os
 import torch
